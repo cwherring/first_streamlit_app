ebec1fd75b62529b7a1c07284167b6a98dfe358b
Chris <cwherring@users.noreply.github.com>
2022-05-25 10:59:09 -0500

    fixed error with multiselect

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,5 +13,5 @@ streamlit.text('🥑🍞 Avocado Toast')
 
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
-streamlit.multiselect('Pick some fruits: ', my_fruit_list.index))
+streamlit.multiselect('Pick some fruits: ', list(my_fruit_list.index))
 streamlit.dataframe(my_fruit_list)
